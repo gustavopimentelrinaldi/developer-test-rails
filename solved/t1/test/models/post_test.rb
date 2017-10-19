0b5ac958e8f01c5f184e361a576efc8ac0ecbebb
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2017-10-19 10:58:01 -0200

    TDD Inicializado

--- solved/t1/test/models/post_test.rb
+++ solved/t1/test/models/post_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be invalid without a title" do
+    posts(:one).title = nil
+    assert_nil posts(:one).title
+    assert_equal false, posts(:one).valid?
+  end
+
+  test "should be invalid without a body" do
+    posts(:two).body = nil
+    assert_nil posts(:two).body
+    assert_equal false, posts(:two).valid?
+  end
 end
